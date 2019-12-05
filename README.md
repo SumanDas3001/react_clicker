d40634ff24f0c51435a5ebc7803bb649dc2742dd
SumanDas3001 <sumandas3001@gmail.com>
2019-12-05 12:31:41 +0530

    Integrate click functionality and Added app image

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![Imgur Image](http://public/app_image.png)
+
 ### react_clicker
 > A basic React app that allows one to increase, decrease, or reset a counter.
 
--- src/components/Clicker.js
+++ src/components/Clicker.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { directive } from '@babel/types';
+import PropTypes from 'prop-types';
+
 
 class Clicker extends Component {
 
@@ -9,6 +10,22 @@ class Clicker extends Component {
     this.state = {
       count: props.count
     };
+
+    this.incrementCount = this.incrementCount.bind(this);
+    this.decrementCount = this.decrementCount.bind(this);
+    this.resetCount = this.resetCount.bind(this);
+  }
+
+  incrementCount (){
+    this.setState((prevState) => ({ count: prevState.count + 1 }));
+  }
+    
+  decrementCount (){
+    this.setState((prevState) => ({ count: prevState.count - 1 }));
+  }
+
+  resetCount () {
+    this.setState(() => ({ count: 0 }));
   }
 
   render(){
@@ -17,12 +34,28 @@ class Clicker extends Component {
         <div className="clicker border border-secondary rounded">
           <div className="mx-auto display-1">{this.state.count}</div>
         </div>
-        <div className="clicker-button-panel d-flex flex-row">
-
+        <div className="clicker-button-panel d-flex flex-row button_padding">
+          <button className="btn btn-success w-100" onClick={this.incrementCount}>
+            <i className="fa fa-plus fa-2x"></i>
+          </button>
+          <button className="btn btn-warning w-100" onClick={this.resetCount}>
+            <i className="fa fa-refresh fa-2x"></i>
+          </button>
+          <button className="btn btn-danger w-100" onClick={this.decrementCount}>
+            <i className="fa fa-minus fa-2x"></i>
+          </button>
         </div>
       </div>
     );
   }
 }
+
+Clicker.defaultProps = {
+  count: 0
+};
+
+Clicker.propTypes = {
+  count: PropTypes.number
+};
 
 export default Clicker;
